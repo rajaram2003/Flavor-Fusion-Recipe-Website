4b9c562ba0ece2f772e11abb7db3d9f2c162a339
rajaram2003 <rajarambiswal123@gmail.com>
2024-10-17 11:10:29 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import Recipes from '../components/Recipes'
+
+const Home = () => {
+  return (
+    <main className='w-full flex flex-col'>
+      <Header 
+      title={<p>Delight Your Palate with<br />FlavorFusion!</p>}
+      type='home'/>
+
+    <section id="recipes" className='md:max-w-[1440px] mx-auto px-4 md:px-20'>
+     <Recipes />
+    </section>
+
+    </main> 
+  )
+}
+
+export default Home;
